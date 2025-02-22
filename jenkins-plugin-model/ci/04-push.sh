9a5df8e53aa34bc34a01c8a5ae81fa7d1c6f9842
chetan23296 <chetansakunde96@gmail.com>
2025-02-22 11:35:20 +0530

    Update 04-push.sh

--- jenkins-plugin-model/ci/04-push.sh
+++ jenkins-plugin-model/ci/04-push.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-docker image build -t chetan1823/sha256:9f885012075ae29f0be35acee2cd7b3755083ea495e6c52f769ae1ea115d4d40:$1 -f jenkins-plugin-model/src/demo2-publish/dockerfile .
+docker image build -t chetan1823/sha256:$1 -f jenkins-plugin-model/src/demo2-publish/dockerfile .
 
 if [ -z ${DOCKER_HUB_USER+x} ]
 then 
@@ -8,4 +8,4 @@ else
     docker login -u $DOCKER_HUB_USER -p $DOCKER_HUB_PASSWORD
 fi
 
-docker push chetan1823/sha256:9f885012075ae29f0be35acee2cd7b3755083ea495e6c52f769ae1ea115d4d40:$1
+docker push chetan1823/sha256:$1
